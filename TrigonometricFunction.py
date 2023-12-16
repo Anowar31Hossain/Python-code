b4feeedc0af63f4f0dd20478a25f1cb08f6905e5
Anowar31Hossain <mdanowarnoyon7075@gmail.com>
2023-12-16 15:16:43 +0600

    added how use trigonometric function in python

---
+++ TrigonometricFunction.py
@@ -0,0 +1,9 @@
+import math
+angle_in_degree=int(input("Enter a degree what you want:"))
+angle_in_radian=math.radians(angle_in_degree)
+sin_value=math.sin(angle_in_radian)
+cos_value=math.cos(angle_in_radian)
+tan_value=math.tan(angle_in_radian)
+print(sin_value)
+print(cos_value)
+print(tan_value)
