63f965845f95abc44c73bd18634dcfaf1bfc1da3
Anowar31Hossain <mdanowarnoyon7075@gmail.com>
2024-01-25 18:39:21 +0600

    pdf to doc convert code added

--- ConvertPDFtoDocs.py
+++ ConvertPDFtoDocs.py
@@ -8,8 +8,8 @@ def pdf_to_word_converter(input_pdf_path,output_doc_path):
         text = page.extract_text()
         doc.add_paragraph(text)
     doc.save(output_doc_path)
-    print(f"PDF successfully converted to the word document.Output.doc save at:{output_doc_path}")
+    print(f"PDF successfully converted to the word document. Output.doc save at:{output_doc_path}")
 
 input_pdf_path = r"C:\Users\ACER\Downloads\Lab Problems on ARRAY.pdf"
 output_doc_path = r"C:\Users\ACER\Downloads\output.doc"
-pdf_to_word_converter(input_pdf_path,output_doc_path)
+pdf_to_word_converter(input_pdf_path,output_doc_path) 
